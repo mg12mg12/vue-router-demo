37cb1aa9ad8529b49755096f5ee240e9e955ab06
MGMG <a20819z@gmail.com>
2025-04-20 20:34:43 +0800

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+      <h2>📖 關於我們</h2>
+      <p>這是一個用 Vue Router 建立的測試頁面。</p>
+    </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+      <h2>🏠 這是首頁</h2>
+      <p>歡迎來到我們的網站！</p>
+    </div>
+</template>
